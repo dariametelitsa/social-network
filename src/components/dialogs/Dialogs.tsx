7eda6d3aa93d918317dc89845cb4739f1ab13240
dariametelitsa <dariametelitsa@gmail.com>
2024-04-21 12:51:26 +0300

    added new component Dialog

---
+++ src/components/dialogs/Dialogs.tsx
@@ -0,0 +1,14 @@
+// @flow 
+import * as React from 'react';
+import s from './Dialogs.module.scss';
+
+type Props = {
+    
+};
+export const Dialogs = (props: Props) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+};
