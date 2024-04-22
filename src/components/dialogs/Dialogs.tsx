252f44945e970405b66ddd87bbd45ce168031729
dariametelitsa <dariametelitsa@gmail.com>
2024-04-22 17:42:45 +0300

    added first dialogs draft

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -7,8 +7,20 @@ type Props = {
 };
 export const Dialogs = (props: Props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+                <ul className={s.dialogsItems}>
+                    <li className={s.dialog + ' ' + s.active}>Tom</li>
+                    <li className={s.dialog}>Steve</li>
+                    <li className={s.dialog}>Veronica</li>
+                    <li className={s.dialog}>Nataniel</li>
+                    <li className={s.dialog}>Mike</li>
+                    <li className={s.dialog}>Aurora</li>
+                </ul>
+                <ul className={s.messagesItems}>
+                    <li className={s.message}>Hey!</li>
+                    <li className={s.message}>How are you?</li>
+                    <li className={s.message}>Do you want to go to the concert with me?</li>
+                </ul>
         </div>
     );
 };
