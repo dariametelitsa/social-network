fe90f587a79ec68515b9895fc75bd5d54ba6614a
Daria Metelitsa <dariametelitsa@gmail.com>
2024-08-26 19:06:45 +0300

    refactored reducers async logic

--- src/redux/thunks/profileThunks.ts
+++ src/redux/thunks/profileThunks.ts
@@ -6,29 +6,34 @@ import { profileAPI } from "../../api/profileAPI";
 import { setUserProfile, setUserStatus } from "../profileReducer";
 import axios, { AxiosError } from "axios";
 
-export const getUserProfileTC = (userId: number): ThunkActionType => (dispatch) => {
+export const getUserProfileTC = (userId: number): ThunkActionType => async (dispatch) => {
     dispatch(toggleFollowingUser(userId, true));
-    profileAPI.getUserProfile(userId)
-        .then(res => {
-            dispatch(setUserProfile(res));
-        })
-        .catch(e => {
-            console.log(e.message)
-        })
+    try {
+        const res = await profileAPI.getUserProfile(userId)
+        dispatch(setUserProfile(res));
+    } catch (e: unknown){
+        if (axios.isAxiosError(e)) {
+            console.log(e.message);
+        } else {
+            console.log((e as Error).message);
+        }
+    }
 }
 
-export const getUserStatusTC = (userId: number): ThunkActionType => (dispatch) => {
-    profileAPI.getUserStatus(userId)
-        .then(res => {
-            dispatch(setUserStatus(res.data));
-        })
-        .catch(e => {
+export const getUserStatusTC = (userId: number): ThunkActionType => async (dispatch) => {
+    try {
+        const res = await profileAPI.getUserStatus(userId);
+        dispatch(setUserStatus(res.data));
+    } catch (e: unknown) {
+        if (axios.isAxiosError(e)) {
             console.log(e.message);
-        })
+        } else {
+            console.log((e as Error).message);
+        }
+    }
 }
 
 export const updateUserStatusTC = (status: string): ThunkActionType<Promise<void>> => async dispatch => {
-    const res = await profileAPI.updateUserStatus(status);
     try {
         const res = await profileAPI.updateUserStatus(status);
         if (res.data.resultCode === 0) {
